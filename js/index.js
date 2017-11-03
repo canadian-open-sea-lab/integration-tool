b43d6f5feb6095f640b053e7da067c0e02b62403
desjardinsa <desjardinsa@ogsl.ca>
2017-11-03 16:37:22 -0400

    Initialize basic project setup

---
+++ js/index.js
@@ -0,0 +1,9 @@
+function testFunction() {
+  let i = 0;
+  i += 2;
+  console.log(i);
+}
+
+$(document).ready(() => {
+  testFunction();
+});
---
+++ webpack.common.js
@@ -0,0 +1,24 @@
+/* eslint-disable */
+var path = require('path');
+
+module.exports = {
+  entry: './js/index.js',
+  output: {
+    path: path.resolve(__dirname, 'build'),
+    filename: 'index.bundle.js'
+  },
+  module: {
+    loaders: [
+      {
+        test: /\.js$/,
+        loader: 'babel-loader',
+        query: {
+          presets: ['es2015']
+        }
+      }
+    ]
+  },
+  stats: {
+    colors: true
+  }
+};
